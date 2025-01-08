c65823ffeda350ab502504d1338f7d40d0b9ad28
ArturoJMM <slipknot25256@gmail.com>
2025-01-08 14:55:26 -0500

    Excercise 2 | Core Requirement N°1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            Console.WriteLine($"Your Grade is: {letter}");
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+            Console.WriteLine($"Your Grade is: {letter}");
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+            Console.WriteLine($"Your Grade is: {letter}");
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+            Console.WriteLine($"Your Grade is: {letter}");
+        }
+        else
+        {
+            letter = "F";
+            Console.WriteLine($"Your Grade is: {letter}");
+        }
+
+    
     }
 }
