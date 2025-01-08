dfc3f4f9863db8ec53f79eb2a3f03fcf098b92b9
ArturoJMM <slipknot25256@gmail.com>
2025-01-08 15:43:06 -0500

    Stretch Callenges Completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -3,13 +3,18 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   // Core Requirements for the excercise
         Console.Write("What is your grade percentage? ");
         string userInput = Console.ReadLine();
+        
+        //Assigning  Variables and Types
         int grade = int.Parse(userInput);
-
+        
         string letter = "";
+        
+        int lastDigit = grade % 10;
 
+        // Start of the Conditionals
         if (grade >= 90)
         {
             letter = "A";
@@ -31,7 +36,27 @@ class Program
             letter = "F";
         }
 
-        Console.WriteLine($"Your Grade is: {letter}");
+        // Stretch Challenge
+        string sign = "";
+        
+        // Stretch Challenge 2 & 3. Conditionals for A and F letters
+        if (grade > 60 && grade < 97)
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit <= 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+        // Printing the results on the Console
+        Console.WriteLine($"Your Grade is: {letter}{sign}");
 
         if (grade >= 70)
         {
