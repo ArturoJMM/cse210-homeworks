3dab08e628e28bcec4a93a200342a59b7fff83e8
ArturoJMM <slipknot25256@gmail.com>
2025-01-10 17:02:15 -0500

    Core Requirements 2 & 3, and Stretch N°1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,29 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        int magicNumber = 6;
+        Random randomGenerator = new Random(); 
+        int magicNumber = randomGenerator.Next(1, 101);
+        int userNumber = 0;
+        int count = 0;
 
-        Console.Write("Please guess what is the magic number: ");
-        string userInput = Console.ReadLine();
-        int userNumber = int.Parse(userInput);
-
-        Console.WriteLine($"What is the magic number? {magicNumber}");
-        Console.WriteLine($"What is your guess? {userNumber}");
-
-        if (userNumber < magicNumber)
+        while (userNumber != magicNumber)
         {
-            Console.WriteLine("You need to choose a Higher Number");
+            Console.Write("Please guess what is the magic number: ");
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+            count += count;
+
+            Console.WriteLine($"What is your guess? {userNumber}");
+
+            if (userNumber < magicNumber)
+            {
+                Console.WriteLine("You need to choose a Higher Number");
+            }
+
+            else if (userNumber > magicNumber)
+            {
+                Console.WriteLine("You need to choose a Lower Number");
+            }
         }
 
-        else if (userNumber > magicNumber)
-        {
-            Console.WriteLine("You need to choose a Lower Number");
-        }
-
-        else
-        {
-            Console.WriteLine("Great, You guessed It!");
-        }
-
+        Console.WriteLine($"Great, the magic number was {magicNumber}. You guessed It!");
+        Console.WriteLine($"It took you {count} tries to guess It");
+        
     }
 }
