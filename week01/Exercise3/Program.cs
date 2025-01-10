b44ec65565080e31276daafad33ea645afbf8299
ArturoJMM <slipknot25256@gmail.com>
2025-01-10 16:33:05 -0500

    Core Requirement N°1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNumber = 6;
+
+        Console.Write("Please guess what is the magic number: ");
+        string userInput = Console.ReadLine();
+        int userNumber = int.Parse(userInput);
+
+        Console.WriteLine($"What is the magic number? {magicNumber}");
+        Console.WriteLine($"What is your guess? {userNumber}");
+
+        if (userNumber < magicNumber)
+        {
+            Console.WriteLine("You need to choose a Higher Number");
+        }
+
+        else if (userNumber > magicNumber)
+        {
+            Console.WriteLine("You need to choose a Lower Number");
+        }
+
+        else
+        {
+            Console.WriteLine("Great, You guessed It!");
+        }
+
     }
 }
