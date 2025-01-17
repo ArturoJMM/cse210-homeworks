29f3fac803f467de8c024bf052f01a324eef917e
ArturoJMM <slipknot25256@gmail.com>
2025-01-16 22:08:50 -0500

    Completed the option 1 and 2 of the menu

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,15 +1,19 @@
 public class Journal
 {
-    public List<Entry> _entries;
+    public List<Entry> _entries = new List<Entry>();
+    public string file = "Journal.txt";
 
     public void AddEntry(Entry newEntry)
     {
-
+        _entries.Add(newEntry);
     }
     
     public void DisplayAll()
     {
-
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
     }
     
     public void SaveToFile(string file)
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,22 +1,48 @@
 using System;
+using System.Collections.Generic; 
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        PromptGenerator promptGenerator = new PromptGenerator();
-
-        string prompt = promptGenerator.GetRandomPrompt();
-        Console.WriteLine(prompt);
-
         Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+        
+        bool quit = false;
+        while (!quit)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
 
-        Console.WriteLine("Please select one of the following choices:");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Save");
-        Console.WriteLine("4. Load");
-        Console.WriteLine("5. Quit");
+            int choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+                string response = Console.ReadLine();
+                Entry entry = new Entry(DateTime.Now, prompt, response);
+                journal.AddEntry(entry);
+                //entry.Display();
+            }
+
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+
+            else if (choice == 3)
+            {
+                
+            }
+
+        }
 
     }
 }
