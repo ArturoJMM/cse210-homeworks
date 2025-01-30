929dd540e765ebad2621ba13a6a78988aa88fe8a
ArturoJMM <slipknot25256@gmail.com>
2025-01-29 19:36:05 -0500

    W04 Assignment: YouTube Video Program

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        Console.WriteLine();
+
+        Video video1 = new Video("Class with Benji Monroy | “The Hearts of Children Will Turn to Parents” | D&C 2", "Scripture Central", 3997);
+
+        video1.AddComment(new Comment("@ibismoraza8355", "Hello Benji, my dear young teacher. Your classes inspire us. I read Come Follow Me daily."));
+        video1.AddComment(new Comment("@mariatamayo7992", "Thank you brother Benji for the messages that leave a lot of reflection in our lives that helps us improve within the many experiences we have day to day."));
+        video1.AddComment(new Comment("@ediceiamartinez3971", "Hello brother, thank you for this wonderful class and your enlightening drawings."));
+
+        Video video2 = new Video("Class with Benji Monroy | “We Have Entered into a Covenant with Him” | Mosiah 18-24", "Scripture Central", 4130);
+
+        video2.AddComment(new Comment("@denicegore3620", "Hello my friend Benji, thanks for this class that completes the come follow me and how good it is to know that you are well"));
+        video2.AddComment(new Comment("@miriamreiser9145", "Hello my beloved benji happy rest day and also to my beautiful brothers"));
+        video2.AddComment(new Comment("@mariajara142", "Thank you brother for your words of comfort and hope in God."));
+
+        Video video3 = new Video("Class with Benji Monroy | “They were called the people of God” | Mosiah 25-28", "Scripture Central",4471);
+
+        video3.AddComment(new Comment("@trovador1107", "I look forward to the next presentation. Blessings and greetings from Peru."));
+        video3.AddComment(new Comment("@angelamoya8047", "We are not perfect, but the Lord is molding us"));
+        video3.AddComment(new Comment("@sandrapineda9873", "Brother Benji, thank you for your beautiful classes, they help me a lot."));
+        video3.AddComment(new Comment("@fortunataorellana", "I too wonder what my life would be like without the gospel in my life or the lives of my children."));
+
+        List<Video> videos = new List<Video> {video1, video2, video3};
+
+        foreach (var video in videos)
+        {
+            video.DisplayVideoInfo();
+            Console.WriteLine($"Number of Comments: {video.GetCommentCount()}");
+
+            foreach (var comment in video.GetComments())
+            {
+                video.DisplayComment(comment);
+            }
+            Console.WriteLine();
+        }
+
     }
 }
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,48 @@
+public class Video
+{
+    // Properties to track the title, author, and length of the video.
+    private string _title;
+    private string _author;
+    private int _videoLength;
+    
+     // List to store the comments related to the video.
+    private List<Comment> comments = new List<Comment>();
+
+    // Constructor to initialize the video.
+    public Video(string title, string author, int videoLength)
+    {
+        _title = title;
+        _author = author;
+        _videoLength = videoLength;
+    }
+
+    public void DisplayVideoInfo()
+    {
+        string videoData = $"Title: {_title} \nAuthor: {_author}\nLength: {_videoLength} seconds";
+        Console.WriteLine(videoData);
+    }
+
+    // Method to add a comment to the video.
+    public void AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+     // Method to return the number of comments.
+    public int GetCommentCount()
+    {
+        return comments.Count;
+    }
+
+     // Method to get the comments.
+    public List<Comment> GetComments()
+    {
+        return comments;
+    }
+
+    public void DisplayComment(Comment comment)
+    {
+        Console.WriteLine($"The user {comment.GetCommenterName()} wrote: '{comment.GetText()}'");
+    }
+
+}
