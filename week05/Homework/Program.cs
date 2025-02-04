a6e915f0385f14b87c566c3f506950d2d0719472
ArturoJMM <slipknot25256@gmail.com>
2025-02-04 16:17:26 -0500

    Principle of inheritance by creating a base class and derived classes.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        WritingAssignment writing1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInfo());
     }
 }
