0a7a0f2d1183d10669fb35218b6c9f8a9f77838c
ArturoJMM <slipknot25256@gmail.com>
2025-02-11 16:05:53 -0500

    Test the Square class

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square square1 =  new Square("blue", 25);
+
+        string color1 = square1.GetColor();
+
+        double area1 = square1.GetArea();
+
+        Console.WriteLine($"The square of color {color1}, has an area of {area1} cm2");
     }
 }
